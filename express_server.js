9c563927c97cc2a0d32a935f50d39f8d2ce3c111
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-05 19:56:56 +0000

    added ownership to urls/:id pages

--- express_server.js
+++ express_server.js
@@ -60,11 +60,33 @@ function emailExists(email) {
   return false
 }
 
-//FUNCTION TO POPULATE ARRAY WITH ALL URLS CONNECTED TO USER ID
-function findUser(userToBeFound) {
+//FUNCTION TO CHECK IF SHORT URL IS REAL
+function urlExists(shortUrl) {
+  for (var user in urlDatabase) {
+    if (shortUrl === urlDatabase[user].shortUrl) {
+      return true;
+    }
+  }
+  return false
+}
+
+//FUNCTION TO FIND if USER OWNS URL
+function findUrl(shortUrl) {
   matchingUrls = []
   for (var currentUser in urlDatabase) {
-    if (userToBeFound === urlDatabase[currentUser].userID){
+    if (shortUrl === urlDatabase[currentUser].shortUrl){
+      matchingUrls.push(urlDatabase[currentUser].userID)
+    }
+  }
+  return matchingUrls
+}
+
+
+//FUNCTION TO FIND IF short URL EXISTS
+function findUser(shortURL) {
+  matchingUrls = []
+  for (var currentUser in urlDatabase) {
+    if (shortURL === urlDatabase[currentUser].userID){
       matchingUrls.push(urlDatabase[currentUser])
     }
   }
@@ -79,7 +101,7 @@ app.get("/urls/new", (req, res) => {
     }
     res.render("urls_new", templateVars)
   } else {
-    res.redirect("/register")
+    res.redirect("/urls/login")
   };
 });
 
@@ -88,7 +110,6 @@ app.put("/urls/:id/update", function (req,res) {
   if(urlDatabase[req.params.id].userID === req.session.user_id){
     urlDatabase[req.params.id].longUrl = (req.body.updateURL)
   res.redirect("/urls")
-  console.log(req.method)
   } else {
     res.send("Do not have permission to update this url")
   }
@@ -99,7 +120,6 @@ app.delete("/urls/:id/delete", function (req, res) {
   if(urlDatabase[req.params.id].userID === req.session.user_id){
     delete urlDatabase[req.params.id]
     res.redirect("/urls")
-    console.log(req.method)
   } else {
     res.send("You don't have permission to delete this")
   }
@@ -148,26 +168,41 @@ app.post("/logout", function (req, res){
 
 //URL Page for specific url
 app.get("/urls/:id", function (req, res) {
-  if(urlDatabase[req.params.id].userID === req.session.user_id){
+  findUrl(req.params.id) // checks to see who owns url
+  let ownUrl = false
+  for (let i = 0; i < matchingUrls.length; i++) {
+    if (matchingUrls[i] === req.session.user_id)
+    ownUrl = true;  //check to see if user matches cookie
+  }
+
+  if (req.session.user_id == false) {
+    res.send("Please log in to view urls")
+  } else if (urlExists(req.params.id) === false) {
+    res.send("requested short url does not exist")
+  } else if (ownUrl === false) {
+    res.send("you do not own the url")
+  } else  {
     let templateVars = {
       shortUrl: req.params.id,
       fullUrl: urlDatabase[req.params.id],
       user: users[req.session.user_id],
     }
     res.render("urls_show", templateVars);
-  } else{
-    res.send("Please log in to see your Urls");
   }
 });
 
 //List of URL Pages and Homepage
 app.get("/urls", function (req, res) {
-  findUser(req.session.user_id)
-  let templateVars = {
-    urls: matchingUrls,
-    user: users[req.session.user_id],
+    if(req.session.user_id){
+    findUser(req.session.user_id)
+    let templateVars = {
+      urls: matchingUrls,
+      user: users[req.session.user_id],
+    }
+    res.render("urls_index", templateVars);
+  } else {
+    res.send("Please log in to see your Urls");
   }
-  res.render("urls_index", templateVars);
 });
 
 
@@ -179,7 +214,7 @@ app.post("/urls", (req, res) => {
     longUrl: req.body.longURL,
     userID: req.session.user_id
   }
-  res.redirect('urls/' + rand);
+  res.redirect('urls');
 });
 
 //Sends user to actual long url
@@ -234,7 +269,11 @@ app.post("/register", function (req,res){
 
 //HOMEPAGE
 app.get("/", (req, res) => {
-  res.render("urls_home")
+  if(req.session.user_id){
+    res.redirect("/urls")
+  } else {
+    res.redirect("/urls/login")
+  }
 });
 
 app.listen(PORT, () => {
