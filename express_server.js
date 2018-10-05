7e9a0a892ff9347710365348ccf909404075fc78
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-05 22:08:59 +0000

    updated ejs templates to have consistent style and refactored code

--- express_server.js
+++ express_server.js
@@ -3,18 +3,18 @@ var app = express();
 var PORT = 8080; // default port 8080
 
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 const bcrypt = require('bcrypt');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 const cookieSession = require('cookie-session');
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 app.use(cookieSession({
   name: 'session',
   secret: 'my secret string'
-}))
+}));
 
 
 
@@ -38,16 +38,16 @@ const users = {
       "email": "email",
       "password": "password"
     }
-  }
+  };
 
 //Generates Random 6 digit Alphanumeric code for short url or userID
 function generateRandomString() {
-  let randString = ""
-  let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  let randString = "";
+  let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
-    randString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length))
+    randString += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
   }
-  return randString
+  return randString;
 }
 
 //FUNCTION CHECKS IF EMAIL ALREADY EXISTS IN USERS OBJECT
@@ -57,7 +57,7 @@ function emailExists(email) {
       return true;
     }
   }
-  return false
+  return false;
 }
 
 //FUNCTION TO CHECK IF SHORT URL IS REAL
@@ -67,30 +67,39 @@ function urlExists(shortUrl) {
       return true;
     }
   }
-  return false
+  return false;
+}
+
+//FUNCTION to check owner of URL
+function ownUrl(userID) {
+  for (let i = 0; i < matchingUrls.length; i++) {
+    if (matchingUrls[i] === userID)
+    return true; //check to see if user matches cookie
+  }
+  return false;
 }
 
 //FUNCTION TO FIND if USER OWNS URL
 function findUrl(shortUrl) {
-  matchingUrls = []
+  matchingUrls = [];
   for (var currentUser in urlDatabase) {
     if (shortUrl === urlDatabase[currentUser].shortUrl){
-      matchingUrls.push(urlDatabase[currentUser].userID)
+      matchingUrls.push(urlDatabase[currentUser].userID);
     }
   }
-  return matchingUrls
+  return matchingUrls;
 }
 
 
 //FUNCTION TO FIND IF short URL EXISTS
 function findUser(shortURL) {
-  matchingUrls = []
+  matchingUrls = [];
   for (var currentUser in urlDatabase) {
     if (shortURL === urlDatabase[currentUser].userID){
-      matchingUrls.push(urlDatabase[currentUser])
+      matchingUrls.push(urlDatabase[currentUser]);
     }
   }
-  return matchingUrls
+  return matchingUrls;
 }
 
 //CREATES A NEW URL
@@ -98,95 +107,92 @@ app.get("/urls/new", (req, res) => {
   if (req.session.user_id) {
     let templateVars = {
       user: users[req.session.user_id]
-    }
-    res.render("urls_new", templateVars)
+    };
+    res.render("urls_new", templateVars);
   } else {
-    res.redirect("/urls/login")
-  };
+    res.redirect("/urls/login");
+  }
 });
 
 //Updates an existing Long url -- Tied to User ID now
 app.put("/urls/:id/update", function (req,res) {
   if(urlDatabase[req.params.id].userID === req.session.user_id){
-    urlDatabase[req.params.id].longUrl = (req.body.updateURL)
-  res.redirect("/urls")
+    urlDatabase[req.params.id].longUrl = (req.body.updateURL);
+    res.redirect("/urls");
   } else {
-    res.send("Do not have permission to update this url")
+    res.send("Do not have permission to update this url");
   }
-})
+});
 
 //DELETES A URL FROM LIST (CHANGE TO METHOD OVERRIDE EVENTUALLY)
 app.delete("/urls/:id/delete", function (req, res) {
   if(urlDatabase[req.params.id].userID === req.session.user_id){
-    delete urlDatabase[req.params.id]
-    res.redirect("/urls")
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
   } else {
-    res.send("You don't have permission to delete this")
+    res.send("You don't have permission to delete this");
   }
 });
 
 //LOG IN
 app.post("/urls/login", function (req, res) {
-  let email = req.body.email
-  let emailExist = false
-  let passwordExist = false
+  let email = req.body.email;
+  let emailExist = false;
+  let passwordExist = false;
   for (var user in users) {
     if (users[user].email === email){
-      emailExist = true
+      emailExist = true;
       if(bcrypt.compareSync((req.body.password), users[user].password)){
-        passwordExist = true
-        req.session.user_id = users[user].id
-        res.redirect("/urls")
+        passwordExist = true;
+        req.session.user_id = users[user].id;
+        res.redirect("/urls");
       }
     }
   }
   // WHAT GETS SENT ON ERROR MESSAGES
   if (!emailExist){
-    res.statusCode = 403
-    res.send('Error: This account does not exist please register email')
+    res.statusCode = 403;
+    res.send('Error: This account does not exist please register email');
   }
   if (!passwordExist) {
-    res.statusCode = 403
-    res.send('Error: Incorrect Password')
+    res.statusCode = 403;
+    res.send('Error: Incorrect Password');
   }
-})
+});
 
 //Handles Showing Login Page
 app.get("/urls/login", function (req, res){
- let templateVars = {
-    user: users[req.session.user_id]
+  if(req.session.user_id) {
+    res.redirect("/urls");
+  } else {
+    let templateVars = {
+      user: users[req.session.user_id]
+    };
+    res.render("urls_login", templateVars);
   }
-  res.render("urls_login", templateVars);
 });
 
 //Logs out of website and clears the user_ID cookie
 app.post("/logout", function (req, res){
-  req.session = null
-  console.log("YOU HAVE LOGGED OUT")
-  res.redirect("/")
-})
+  req.session = null;
+  res.redirect("/");
+});
 
 //URL Page for specific url
 app.get("/urls/:id", function (req, res) {
-  findUrl(req.params.id) // checks to see who owns url
-  let ownUrl = false
-  for (let i = 0; i < matchingUrls.length; i++) {
-    if (matchingUrls[i] === req.session.user_id)
-    ownUrl = true;  //check to see if user matches cookie
-  }
-
-  if (req.session.user_id == false) {
-    res.send("Please log in to view urls")
+  findUrl(req.params.id); // checks to see who owns url
+  if (req.session.user_id === false) {
+    res.send("Please log in to view urls");
   } else if (urlExists(req.params.id) === false) {
-    res.send("requested short url does not exist")
-  } else if (ownUrl === false) {
-    res.send("you do not own the url")
+    res.send("Error: Requested short url does not exist");
+  } else if (!ownUrl(req.session.user_id)) {
+    res.send("Error: you do not own the url");
   } else  {
     let templateVars = {
       shortUrl: req.params.id,
       fullUrl: urlDatabase[req.params.id],
       user: users[req.session.user_id],
-    }
+    };
     res.render("urls_show", templateVars);
   }
 });
@@ -194,11 +200,11 @@ app.get("/urls/:id", function (req, res) {
 //List of URL Pages and Homepage
 app.get("/urls", function (req, res) {
     if(req.session.user_id){
-    findUser(req.session.user_id)
+    findUser(req.session.user_id);
     let templateVars = {
       urls: matchingUrls,
       user: users[req.session.user_id],
-    }
+    };
     res.render("urls_index", templateVars);
   } else {
     res.send("Please log in to see your Urls");
@@ -208,71 +214,79 @@ app.get("/urls", function (req, res) {
 
 //Creates a random short URL for entered url and adds to list
 app.post("/urls", (req, res) => {
-  let rand = generateRandomString ()
+  let rand = generateRandomString ();
   urlDatabase[rand] = {
     shortUrl: rand,
     longUrl: req.body.longURL,
     userID: req.session.user_id
-  }
+  };
   res.redirect('urls');
 });
 
 //Sends user to actual long url
 app.get("/u/:shortURL", (req, res) => {
-  let realUrl = urlDatabase[req.params.shortURL].longUrl
-  if (realUrl.slice(0,4) === 'http'){
-    res.redirect(realUrl);
+  if (urlExists(req.params.shortURL) === false) {
+    res.send("Error: Requested short url does not exist");
   } else {
-    res.redirect('http://' + realUrl)
+    let realUrl = urlDatabase[req.params.shortURL].longUrl;
+    if (realUrl.slice(0,4) === 'http'){
+      res.redirect(realUrl);
+    } else {
+      res.redirect('http://' + realUrl);
+    }
   }
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-  console.log(res)
+  console.log(res);
 });
 
 
 
 //REGISTRATION INFORMATION
 app.get("/register", function (req, res){
+  if(req.session.user_id) {
+    res.redirect("/urls");
+  } else {
   let templateVars = {
     user: users[req.session.user_id]
-  }
+  };
   res.render("urls_register", templateVars);
+  }
 });
 
 app.post("/register", function (req,res){
-  let email = req.body.email
-  let password = req.body.password
-  let hashedPassword = bcrypt.hashSync(password, 10)
+  let email = req.body.email;
+  let password = req.body.password;
+  let hashedPassword = bcrypt.hashSync(password, 10);
   if (!email || !password) {            //Handles error if either email or password are blank
-    res.statusCode = 400
-    res.send('Error: Email or Password are empty')
-    console.log('Status Code: ', res.statusCode)
+    res.statusCode = 400;
+    res.send('Error: Email or Password are empty');
+    console.log('Status Code: ', res.statusCode);
   } else if (emailExists(email)) {       //Handles error if email already exists
-    res.statusCode = 400
-    res.send('Error: This email has already been registered')
-    console.log('Status Code: ', res.statusCode)
+    res.statusCode = 400;
+    res.send('Error: This email has already been registered');
+    console.log('Status Code: ', res.statusCode);
   } else {
-    let rand = generateRandomString ()
+    let rand = generateRandomString ();
     users[rand] = {
       id: rand,
       email: email,
       password: hashedPassword
-    }
-    console.log("Current User Database" , users)
-    req.session.user_id = rand //Assign cookie to userID
-    res.redirect("/urls")
+    };
+    console.log("Current User Database" , users);
+    req.session.user_id = rand; //Assign cookie to userID
+    res.redirect("/urls");
   }
-})
+});
 
 //HOMEPAGE
 app.get("/", (req, res) => {
   if(req.session.user_id){
-    res.redirect("/urls")
+    res.redirect("/urls");
   } else {
-    res.redirect("/urls/login")
+    res.redirect("/urls/login");
   }
 });
 
